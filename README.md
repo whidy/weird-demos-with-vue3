78843cf37dbe2e9da5e247c55ea5f7974d16902b
whidy <whidy@whidy.net>
2023-01-17 21:14:35 +0800

    init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# 奇怪的Vue.js 3.x
+
+> 什么奇怪，明明太菜！
+
+该项目基于[Vite-Element-Plus-UnoCSS](https://github.com/whidy/Vite-Element-Plus-UnoCSS)模板进行基础搭建。
+
+## 问题集合
+
+* 关于Reactive和Ref相关。
---
+++ vite.config.ts
@@ -0,0 +1,68 @@
+import path from "path";
+import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Inspect from "vite-plugin-inspect";
+
+import Unocss from "unocss/vite";
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+const pathSrc = path.resolve(__dirname, "src");
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": pathSrc,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@use \"@/assets/styles/element/index.scss\" as *;",
+      },
+    },
+  },
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ["vue"],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+        }),
+      ],
+      dts: path.resolve(pathSrc, "typings", "auto-imports.d.ts"),
+    }),
+    Components({
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass",
+        }),
+      ],
+      dts: path.resolve(pathSrc, "typings", "components.d.ts"),
+    }),
+
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+          warn: true,
+        }),
+      ],
+      transformers: [transformerDirectives(), transformerVariantGroup()],
+    }),
+    Inspect(),
+  ],
+});
